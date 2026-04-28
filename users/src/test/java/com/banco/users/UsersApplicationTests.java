9021f2c62b1959a561d3110ec32b588dfc5b7368
Elizabeth Tamayo <dianelizbty@gmail.com>
2026-04-28 12:09:35 -0500

    Initial commit

---
+++ users/src/test/java/com/banco/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
