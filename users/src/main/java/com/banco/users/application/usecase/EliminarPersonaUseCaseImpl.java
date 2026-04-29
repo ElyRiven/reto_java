a83e06fec028bc1d2c838353e4c29b620436768f
Elizabeth Tamayo <dianelizbty@gmail.com>
2026-04-28 22:24:27 -0500

    refactor: update Persona controller to include missing CRUD endpoints; add and update use cases implementations

--- users/src/main/java/com/banco/users/application/usecase/EliminarPersonaUseCaseImpl.java
+++ users/src/main/java/com/banco/users/application/usecase/EliminarPersonaUseCaseImpl.java
@@ -1,7 +1,6 @@
 package com.banco.users.application.usecase;
 
 import com.banco.users.domain.exception.PersonaNotFoundException;
-import com.banco.users.domain.model.Persona;
 import com.banco.users.domain.port.out.PersonaRepositoryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,13 +9,15 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class ConsultarPersonaUseCaseImpl implements ConsultarPersonaUseCase {
+public class EliminarPersonaUseCaseImpl implements EliminarPersonaUseCase {
 
     private final PersonaRepositoryPort personaRepositoryPort;
 
     @Override
-    public Persona execute(UUID id) {
-        return personaRepositoryPort.findById(id)
-                .orElseThrow(() -> new PersonaNotFoundException("No existe una persona con el id indicado"));
+    public void execute(UUID id) {
+        personaRepositoryPort.findById(id)
+                .orElseThrow(() -> new PersonaNotFoundException(
+                        "No existe una persona con el id indicado"));
+        personaRepositoryPort.softDelete(id);
     }
 }
